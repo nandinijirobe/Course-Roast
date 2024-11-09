8c66a6aab617e49c283828baf500298f19efc210
Nandini Jirobe <95235229+nandinijirobe@users.noreply.github.com>
2024-11-09 17:39:08 -0600

    Completed landing page
    Co-authored-by: sdeerod <drodr32@users.noreply.github.com>

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,31 @@
-export default function Landing () {
-    return(
+import './Landing.css';
+
+import React from 'react';
+import './Landing.css'; // Optional: If you prefer to style using an external CSS file
+
+export default function Landing(){
+    return (
         <>
-            this is the landing page
+            <div className="logo"><span className="logo-course">course</span> <span className="logo-roast">roast</span>☕️</div>
+
+            <div className="headline">
+                the <span className="highlight realest">realest</span><br />
+                <span className="highlight course">course review</span> <span className="highlight review">website</span>
+            </div>
+
+            <div className="subtitle">
+                a one stop dashboard to view anonymous <br />feedback for all cs classes<br />
+                <span className="sub-subtitle">for students, from students </span>
+            </div>
+
+            <div className="dashboard-btn-location">
+                <button className="dashboard-btn">dashboard</button>
+            </div>
+            
+
+            <div className="footer">made w &lt;3</div>
+        
         </>
-    )
+
+    );
 }
