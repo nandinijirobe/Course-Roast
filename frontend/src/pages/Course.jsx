0f17ba6b22a73c9e3c9ee00c6d5ebbe98ff32711
Nandini Jirobe <95235229+nandinijirobe@users.noreply.github.com>
2024-11-20 14:06:51 -0600

    Added skeleton of reviews to course page

--- frontend/src/pages/Course.jsx
+++ frontend/src/pages/Course.jsx
@@ -1,7 +1,14 @@
+import styles from './Course.module.css';
+import React from 'react';
+import ReviewsDisplay from './components/ReviewsDisplay'
+import Logo from './components/Logo'
+
+
 export default function Course () {
     return(
         <>
             this is the course page
+            <ReviewsDisplay/>
         </>
     )
 }
