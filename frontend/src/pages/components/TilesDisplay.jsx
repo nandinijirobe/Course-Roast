c58876fa1d3e3201de9f8e857226c213158c89ae
Lisset Rico <lrico5@uic.edu>
2024-11-26 16:12:05 -0600

    tile with sql

--- frontend/src/pages/components/TilesDisplay.jsx
+++ frontend/src/pages/components/TilesDisplay.jsx
@@ -28,24 +28,17 @@ export default function CourseTiles() {
 
     return (
         <div className= {styles["all-tiles"]}>
+
             <Tile courseCode = "CS 111" courseName = "Program Design I" courseType = "Required Elective" courseOverallRating= {4.9}/>
             <Tile courseCode = "CS 112" courseName = "Program Design II" courseType = "Required Elective" courseOverallRating= {4.5}/>
             <Tile courseCode = "CS 151" courseName = "Mathematical Foundations of Computing" courseType = "Required Elective" courseOverallRating= {3.7}/>
             <Tile courseCode = "CS 211" courseName = "Programming Practicum" courseType = "Technical Elective" courseOverallRating= {2.2}/>
             <Tile courseCode = "CS 251" courseName = "Data Structures" courseType = "Technical Elective" courseOverallRating= {1.1}/>
             <Tile courseCode = "CS 477" courseName = "Public Policy, Legal, and Ethical Issues in Computing, Privacy, and Security" courseType = "Technical Elective" courseOverallRating= {0.5}/>
-        {/* <tbody>
-                <tr>
-                    <td><Tile courseCode = "CS 111" courseName = "Program Design I" courseType = "Required Elective" courseOverallRating= {4.9}/></td>
-                    <td><Tile courseCode = "CS 112" courseName = "Program Design II" courseType = "Required Elective" courseOverallRating= {4.5}/></td>
-                    <td><Tile courseCode = "CS 151" courseName = "Mathematical Foundations of Computing" courseType = "Required Elective" courseOverallRating= {3.7}/></td>
-                </tr>
-                <tr>
-                    <td><Tile courseCode = "CS 211" courseName = "Programming Practicum" courseType = "Technical Elective" courseOverallRating= {2.2}/></td>
-                    <td><Tile courseCode = "CS 251" courseName = "Data Structures" courseType = "Technical Elective" courseOverallRating= {1.1}/></td>
-                    <td><Tile courseCode = "CS 477" courseName = "Public Policy, Legal, and Ethical Issues in Computing, Privacy, and Security" courseType = "Technical Elective" courseOverallRating= {0.5}/></td>
-                </tr>
-        </tbody> */}
-    </div>
+
+            {courseData.map(course => (
+                <Tile key={course.code} courseCode={course.code} courseName={course.title} courseType={course.type} courseOverallRating={1.2} />
+            ))}
+        </div>
     );
 }
